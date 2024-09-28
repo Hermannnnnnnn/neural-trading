733ceaa7ca5eb02a0c0c3e0d91c461431f30db17
Herman <hermandezoveelste@gmail.com>
2024-09-28 13:21:42 +0200

    updating my changes

--- helper_raw_data.py
+++ helper_raw_data.py
@@ -24,9 +24,20 @@ class raw_data:
   def download_data(self) -> None:
     self.df_prices = yf.download(self.ticker, start=self.start_date, end=self.end_date, interval=self.interval).reset_index()
 
-  def calculate_percentual_change(self, field='Close', steps=1) -> None:
+  def calculate_percentual_change(self, field: str ='Close', steps: int =1) -> None:
       """
-      function to calculate the percentual change over a number of steps. Defaults to the 'Close' column of the raw downloaded data.
+      This function calculates the percentual change over a number of steps. Defaults to the 'Close' column of the raw downloaded data.
+      --------------------
+      INPUT
+      --------------------
+      field:      This string is the column name in self.df_prices, holding price data for which we want to calculate percentual changes.
+      steps:      This integer tells how many steps in the future we go to compare prices with and calculate the percentual change. 
+      --------------------
+      OUTCOME
+      --------------------
+      Two added columns to self.df_prices.
+      - First is f'shifted_{steps}' which is the {field} column, shifted {steps} steps.
+      - Second is f'perc_ch_{steps}_steps'. It contains the percentual change between f'shifted_{steps}' and {field}
       """
       column_headers = self.df_prices.columns.tolist()
 
@@ -36,9 +47,21 @@ class raw_data:
           self.df_prices[f'shifted_{steps}']       = self.df_prices[field].shift(-steps)
           self.df_prices[f'perc_ch_{steps}_steps'] = (self.df_prices[f'shifted_{steps}'] - self.df_prices[field])/self.df_prices[f'shifted_{steps}']*100
 
-  def bucketting_prices(self,steps=1, bins=[-100,-2,-1,1,2,100],labels = [-2,-1,0,1,2]) -> None:# labels=['Very bad', 'bad', 'neutral', 'good', 'very good']):
+  def bucketting_prices(self,smoothen: bool = True, steps: int =1, bins: list =[-100,-2,-1,1,2,100],labels: list = [-2,-1,0,1,2]) -> None:# labels=['Very bad', 'bad', 'neutral', 'good', 'very good']):
       """
       This function buckets price movements (in percentages) into bins.
       Hence you have to run calculate_percentual_change first.
+      ---------------------
+      INPUT
+      ---------------------
+      smoothen: if true, then the percentual changes are averaged out a bit, in case we have an extremely volatile market.
+      steps:    the same value as used in calculate_percentual_change() and tells for how many steps we look in the future to calculate the percentual increase
+      bins:     list determining boundaries for buckets
+      labels:   list for the labels ie values of the buckets
+      ---------------------
+      OUTCOME
+      ---------------------
+      Adds a column to self.df_prices. Column is named f'perc_ch_{steps}_steps_binned'
       """
-      self.df_prices[f'perc_ch_{steps}_steps_binned'] = pd.cut(self.df_prices[f'perc_ch_{steps}_steps'], bins=bins,labels=labels)
+
+      self.df_prices[f'perc_ch_{steps}_steps_binned'] = pd.cut((self.df_prices[f'perc_ch_{steps}_steps']+self.df_prices[f'perc_ch_{steps}_steps'].shift(1))/2, bins=bins,labels=labels)
